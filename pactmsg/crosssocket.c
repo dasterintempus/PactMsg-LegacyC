56900102a3e3d8309e9db04e3e303b2da3378c87
scottbuettner <buettner.scott@gmail.com>
2014-12-14 19:02:37 -0700

    Finishing crosssocket.c (pact_socket_*)

--- pactmsg/crosssocket.c
+++ pactmsg/crosssocket.c
@@ -1,3 +1,6 @@
+/*
+//This code is heavily based on code from libircclient (http://www.ulduzsoft.com/libircclient/), specifically src/sockets.c
+*/
 #include "crosssockets.h"
 
 int pact_get_last_socket_error() {
@@ -10,6 +13,9 @@ int pact_get_last_socket_error() {
 
 int pact_socket_create(int domain, int type, int blocking, pact_Socket* sock) {
 	*sock = socket(domain, type, 0);
+	if (PACT_CHECK_SOCKET_ERROR(*sock)) {
+		return *sock;
+	}
 
 	if (!blocking) {
 #ifndef PACT_OS_WIN
@@ -20,10 +26,10 @@ int pact_socket_create(int domain, int type, int blocking, pact_Socket* sock) {
 #endif
 		if (fail) {
 			pact_socket_close(sock);
-			return 1;
+			return fail;
 		}
 	}
-	return PACT_CHECK_SOCKET_ERROR(*sock) ? 1 : 0;
+	return 0;
 }
 
 void pact_socket_close(pact_Socket* sock) {
@@ -34,4 +40,84 @@ void pact_socket_close(pact_Socket* sock) {
 #endif
 
 	*sock = -1;
+}
+
+int pact_socket_connect(pact_Socket* sock, const struct sockaddr* saddr, socklen_t len) {
+	//This just loops connect() if it gets EINTR (who needs interrupts anyway)
+	//Otherwise it will return the socket error number
+	int socketreturn = 0;
+	while (1) {
+		if (connect(*sock, saddr, len) < 0)	{
+			socketreturn = pact_get_last_socket_error();
+			if (socketreturn != EINTR) {
+				return socketreturn;
+			}
+			//else keep looping
+		}
+		else {
+			return 0;
+		}
+	}
+	return 0;
+}
+
+int pact_socket_accept(pact_Socket* sock, pact_Socket* newsock, struct sockaddr* saddr, socklen_t* len) {
+	//Same as pact_socket_connect basically
+	int nsocket = 0;
+	int socketreturn = 0;
+	while (1) {
+		nsocket = accept(*sock, saddr, len);
+		if (PACT_CHECK_SOCKET_ERROR(nsocket)) {
+			socketreturn = pact_get_last_socket_error();
+			if (socketreturn != EINTR) {
+				return socketreturn;
+			}
+			//else keep looping
+		}
+		else {
+			*newsock = nsocket;
+			return 0;
+		}
+	}
+	return 0;
+}
+
+int pact_socket_recv(pact_Socket* sock, void *buf, size_t len) {
+	//See pact_socket_connect, except for recv()
+	int length = 0;
+	int socketreturn = 0;
+	while (1) {
+		length = recv(*sock, buf, len, 0);
+		if (PACT_CHECK_SOCKET_ERROR(length)) {
+			socketreturn = pact_get_last_socket_error();
+			if (socketreturn != EINTR) {
+				return socketreturn;
+			}
+			//else keep looping
+		}
+		else {
+			return length;
+		}
+	}
+	return 0;
+}
+
+int pact_socket_send(pact_Socket* sock, const void* buf, size_t len) {
+	//See pact_socket_connect, except for send()
+	int length = 0;
+	int socketreturn = 0;
+	while (1) {
+		length = send(*sock, buf, len, 0);
+		if (PACT_CHECK_SOCKET_ERROR(length)) {
+			socketreturn = pact_get_last_socket_error();
+			if (socketreturn != EINTR) {
+				return socketreturn;
+			}
+			//else keep looping
+		}
+		else {
+			return length;
+		}
+	}
+	return 0;
 }
--- pactmsg/crosssockets.h
+++ pactmsg/crosssockets.h
@@ -1,3 +1,7 @@
+/*
+//This code is heavily based on code from libircclient (http://www.ulduzsoft.com/libircclient/), specifically src/sockets.c
+*/
+
 #ifndef PACT_CROSSSOCKETS_H
 #define PACT_CROSSSOCKETS_H
 
@@ -25,9 +29,26 @@ typedef SOCKET pact_Socket;
 
 #define PACT_CHECK_SOCKET_ERROR(e)	((e)==SOCKET_ERROR)
 
+#if !defined(EWOULDBLOCK)
+#define EWOULDBLOCK		WSAEWOULDBLOCK
+#endif
+#if !defined(EINPROGRESS)
+#define EINPROGRESS		WSAEINPROGRESS
+#endif
+#if !defined(EINTR)
+#define EINTR			WSAEINTR
+#endif
+#if !defined(EAGAIN)
+#define EAGAIN			EWOULDBLOCK
+#endif
+
 #endif
 
 int pact_get_last_socket_error();
 int pact_socket_create(int domain, int type, int blocking, pact_Socket* sock);
 void pact_socket_close(pact_Socket* sock);
+int pact_socket_connect(pact_Socket* sock, const struct sockaddr *saddr, socklen_t len);
+int pact_socket_accept(pact_Socket* sock, pact_Socket* newsock, struct sockaddr* saddr, socklen_t* len);
+int pact_socket_recv(pact_Socket* sock, void* buf, size_t len);
+int pact_socket_send(pact_Socket* sock, const void* buf, size_t len);
 #endif
