35073a0d7f2d26186c9f34d1ad7e9e6dabb3308f
Brent Taylor <btaylor@fuzzylogicstudios.com>
2014-12-20 21:38:46 -0800

    Fixed a bug in pact_linked_list_pushfront.  New node's prev field was not set to NULL. Fixed a bug in pact_linked_list_pushfront.  Error codes were swapped returning false for success and non zero for failure.

--- src/linkedlist.c
+++ src/linkedlist.c
@@ -75,16 +75,17 @@ void* pact_linkedlist_popback(pact_LinkedList* llist) {
 int pact_linkedlist_pushfront(pact_LinkedList* llist, void* in) {
 	pact_LinkedListNode* node = malloc(sizeof(pact_LinkedListNode));
 	if (!node) {
-		return 1;
+		return 0;
 	}
 	memset(node, 0, sizeof(pact_LinkedListNode));
 
 	node->data = in;
+	node->prev = NULL;
 	node->next = llist->head;
 	llist->head->prev = node;
 	llist->head = node;
 	llist->length++;
-	return 0;
+	return 1;
 }
 
 int pact_linkedlist_pushback(pact_LinkedList* llist, void* in) {
