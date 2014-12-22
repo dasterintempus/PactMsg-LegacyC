f71c44902e0125b283322f89937b86586ca77156
Brent Taylor <btaylor@fuzzylogicstudios.com>
2014-12-21 22:59:51 -0800

    Removed old string code.

--- src/pstring.c
+++ src/pstring.c
@@ -4,8 +4,8 @@
 #include <string.h>
 
 struct pact_String {
-	const char* data;
-	const size_t length;
+	char* data;
+	size_t length;
 };
 
 pact_String* pact_string_new(const char* data) {
@@ -96,7 +96,7 @@ int pact_string_compare_cstr(const pact_String* a, const char* b) {
 	}
 }
 
-inline pact_String* pact_string_copy(const pact_String* str) {
+pact_String* pact_string_copy(const pact_String* str) {
 	return pact_string_new(pact_string_copy_cstr(str));
 }
 
@@ -114,23 +114,23 @@ int pact_string_find_after_cstr(const pact_String* str, const char* value, const
 	return -1;
 }
 
-inline int pact_string_find_after(const pact_String* str, const pact_String* value, const size_t offset) {
+int pact_string_find_after(const pact_String* str, const pact_String* value, const size_t offset) {
 	return pact_string_find_after_cstr(str, value->data, offset);
 }
 
-inline int pact_string_find(const pact_String* str, const pact_String* value) {
+int pact_string_find(const pact_String* str, const pact_String* value) {
 	return pact_string_find_after_cstr(str, value->data, 0);
 }
 
-inline int pact_string_find_cstr(const pact_String* str, const char* value) {
+int pact_string_find_cstr(const pact_String* str, const char* value) {
 	return pact_string_find_after_cstr(str, value, 0);
 }
 
-inline pact_String* pact_string_chop_front(const pact_String* str, const size_t length) {
+pact_String* pact_string_chop_front(const pact_String* str, const size_t length) {
 	return pact_string_substr(str, length, pact_string_get_length(str));
 }
 
-inline pact_String* pact_string_chop_back(const pact_String* str, const size_t length) {
+pact_String* pact_string_chop_back(const pact_String* str, const size_t length) {
 	return pact_string_substr(str, 0, pact_string_get_length(str) - length);
 }
 
@@ -160,318 +160,6 @@ pact_LinkedList* pact_string_split_cstr(const pact_String* str, const char* deli
 	return split_list;
 }
 
-inline pact_LinkedList* pact_string_split(const pact_String* str, const pact_String* delim) {
+pact_LinkedList* pact_string_split(const pact_String* str, const pact_String* delim) {
 	return pact_string_split_cstr(str, delim->data);
-}
-
-pact_String* pact_string_create(const char* data) {
-	pact_String* str = malloc(sizeof(pact_String));
-	if (!str) {
-		return 0;
-	}
-	memset(str, 0, sizeof(pact_String));
-	if (data != NULL) {
-		pact_string_assign(str, data);
-	}
-	return str;
-}
-
-void pact_string_destroy(pact_String* str) {
-	if (str->data) {
-		free(str->data);
-	}
-	free(str);
-}
-
-int pact_string_clear(pact_String* str) {
-	return pact_string_assign(str, "");
-}
-
-int pact_string_assign(pact_String* str, const char* data) {
-	unsigned int length = strlen(data);
-	return pact_string_assign_length(str, data, length);
-}
-
-int pact_string_assign_length(pact_String* str, const char* data, unsigned int length) {
-	if (str->data) {
-		free(str->data);
-	}
-	str->data = malloc(length + 1);
-	if (!str->data){
-		return 1;
-	}
-	str->length = length;
-	memcpy(str->data, data, length);
-	str->data[length] = 0;
-	return 0;
-}
-
-const char* pact_string_view(pact_String* str) {
-	return str->data;
-}
-
-int pact_string_retrieve(pact_String* str, char* data, unsigned int length) {
-	if (length > str->length) {
-		length = str->length;
-	}
-	memcpy(data, str->data, length);
-	data[length] = 0;
-	return 0;
-}
-
-int pact_string_retrieve_substr(pact_String* str, char* data, unsigned int start, unsigned int length) {
-	if (start > str->length) {
-		return 1;
-	}
-	if (length + start > str->length) {
-		length = str->length - start;
-	}
-	memcpy(data, str->data + start, length);
-	data[length] = 0;
-	return 0;
-}
-
-unsigned int pact_string_length(pact_String* str) {
-	return str->length;
-}
-
-int pact_string_compare(pact_String* a, pact_String* b) {
-	unsigned int alen = pact_string_length(a);
-	unsigned int blen = pact_string_length(b);
-	if (alen < blen) {
-		return -1;
-	}
-	else if (alen > blen) {
-		return 1;
-	}
-	//alen == blen
-	else if (alen == 0) {
-		return 0;
-	}
-	return memcmp(a->data, b->data, alen);
-}
-
-int pact_string_compare_cstr(pact_String* a, char* b) {
-	unsigned int alen = pact_string_length(a);
-	unsigned int blen = strlen(b);
-	if (alen < blen) {
-		return -1;
-	}
-	else if (alen > blen) {
-		return 1;
-	}
-	//alen == blen
-	else if (alen == 0) {
-		return 0;
-	}
-	return memcmp(a->data, b, alen);
-}
-
-int pact_string_clone(pact_String* src, pact_String* dst) {
-	return pact_string_clone_substr(src, dst, 0, src->length);
-}
-
-int pact_string_clone_substr(pact_String* src, pact_String* dst, unsigned int start, unsigned int length) {
-	if (start > src->length) {
-		return 1;
-	}
-	if (length + start > src->length) {
-		length = src->length - start;
-	}
-	if (dst->data) {
-		free(dst->data);
-	}
-	dst->data = malloc(length + 1);
-	if (!dst->data) {
-		return 1;
-	}
-	dst->length = length;
-	memcpy(dst->data, src->data + start, length);
-	dst->data[length] = 0;
-	return 0;
-}
-
-int pact_string_find(pact_String* str, pact_String* find) {
-	unsigned int i;
-	if (find->length == 0) {
-		return -1;
-	}
-	for (i = 0; i + find->length <=str->length; i++) {
-		if (memcmp(str->data + i, find->data, find->length) == 0) {
-			return i;
-		}
-	}
-	return -1;
-}
-
-int pact_string_find_after(pact_String* str, pact_String* find, unsigned int offset) {
-	unsigned int i;
-	if (find->length == 0) {
-		return -1;
-	}
-	for (i = offset; i + find->length <= str->length; i++) {
-		if (memcmp(str->data + i, find->data, find->length) == 0) {
-			return i;
-		}
-	}
-	return -1;
-}
-
-int pact_string_find_cstr(pact_String* str, char* find) {
-	unsigned int i;
-	unsigned int findlen = strlen(find);
-	if (findlen == 0) {
-		return -1;
-	}
-	for (i = 0; i + findlen <= str->length; i++)
-	{
-		if (memcmp(str->data + i, find, findlen) == 0) {
-			return i;
-		}
-	}
-	return -1;
-}
-
-int pact_string_find_cstr_after(pact_String* str, char* find, unsigned int offset) {
-	unsigned int i;
-	unsigned int findlen = strlen(find);
-	if (findlen == 0) {
-		return -1;
-	}
-	for (i = offset; i + findlen <= str->length; i++)
-	{
-		if (memcmp(str->data + i, find, findlen) == 0) {
-			return i;
-		}
-	}
-	return -1;
-}
-
-int pact_string_partition(pact_String* str, pact_String* delim, pact_String* head, pact_String* tail) {
-	int find = pact_string_find(str, delim);
-	if (find == -1) {
-		return -1;
-	}
-	pact_string_clone_substr(str, head, 0, find);
-	pact_string_clone_substr(str, tail, find + delim->length, str->length - (find + delim->length));
-	return find;
-}
-
-int pact_string_partition_cstr(pact_String* str, char* delim, pact_String* head, pact_String* tail) {
-	int find = pact_string_find_cstr(str, delim);
-	unsigned int delimlen = strlen(delim);
-	if (find == -1) {
-		return -1;
-	}
-	pact_string_clone_substr(str, head, 0, find);
-	pact_string_clone_substr(str, tail, find + delimlen, str->length - (find + delimlen));
-	return find;
-}
-
-int pact_string_append(pact_String* str, pact_String* tail) {
-	int totallength = str->length + tail->length;
-	char* tmpbuf = malloc(str->length); //No need to null terminate this one
-	pact_string_retrieve(str, tmpbuf, str->length);
-	free(str->data);
-	str->data = malloc(totallength + 1);
-	if (!str->data) {
-		return 1;
-	}
-	memcpy(str->data, tmpbuf, str->length);
-	memcpy(str->data + str->length, tail->data, tail->length);
-	str->length = totallength;
-	str->data[totallength] = 0;
-	return 0;
-}
-
-int pact_string_append_cstr(pact_String* str, char* tail) {
-	int taillen = strlen(tail);
-	int totallength = str->length + taillen;
-	char* tmpbuf = malloc(str->length); //No need to null terminate this one
-	pact_string_retrieve(str, tmpbuf, str->length);
-	free(str->data);
-	str->data = malloc(totallength + 1);
-	if (!str->data) {
-		return 1;
-	}
-	memcpy(str->data, tmpbuf, str->length);
-	memcpy(str->data + str->length, tail, taillen);
-	str->length = totallength;
-	str->data[totallength] = 0;
-	return 0;
-}
-
-int pact_string_prepend(pact_String* str, pact_String* head) {
-	int totallength = str->length + head->length;
-	char* tmpbuf = malloc(str->length); //No need to null terminate this one
-	pact_string_retrieve(str, tmpbuf, str->length);
-	free(str->data);
-	str->data = malloc(totallength + 1);
-	if (!str->data) {
-		return 1;
-	}
-	memcpy(str->data + head->length, tmpbuf, str->length);
-	memcpy(str->data, head->data, head->length);
-	str->length = totallength;
-	str->data[totallength] = 0;
-	return 0;
-}
-
-int pact_string_prepend_cstr(pact_String* str, char* head) {
-	int headlen = strlen(head);
-	int totallength = str->length + headlen;
-	char* tmpbuf = malloc(str->length); //No need to null terminate this one
-	pact_string_retrieve(str, tmpbuf, str->length);
-	free(str->data);
-	str->data = malloc(totallength + 1);
-	if (!str->data) {
-		return 1;
-	}
-	memcpy(str->data + headlen , tmpbuf, str->length);
-	memcpy(str->data, head, headlen);
-	str->length = totallength;
-	str->data[totallength] = 0;
-	return 0;
-}
-
-int pact_string_chop_front(pact_String* str, unsigned int chop) {
-	if (str->length <= chop) {
-		return pact_string_clear(str);
-	}
-	pact_String* temp = pact_string_create(NULL);
-	if (!temp) {
-		return -1;
-	}
-	if (pact_string_clone_substr(str, temp, chop, str->length - chop)) {
-		return -2;
-	}
-	//Magical buffer switch!
-	char* buf = str->data;
-	str->data = temp->data;
-	temp->data = buf;
-	temp->length = str->length; //correct length on decoy str
-	str->length = str->length - chop; //correct length on real str
-	pact_string_destroy(temp); //destroy decoy str
-	return 0;
-}
-
-int pact_string_chop_back(pact_String* str, unsigned int chop) {
-	if (str->length <= chop) {
-		return pact_string_clear(str);
-	}
-	pact_String* temp = pact_string_create(NULL);
-	if (!temp) {
-		return -1;
-	}
-	if (pact_string_clone_substr(str, temp, 0, str->length - chop)) {
-		return -2;
-	}
-	//Magical buffer switch!
-	char* buf = str->data;
-	str->data = temp->data;
-	temp->data = buf;
-	temp->length = str->length; //correct length on decoy str
-	str->length = str->length - chop; //correct length on real str
-	pact_string_destroy(temp); //destroy decoy str
-	return 0;
 }
--- src/pstring.h
+++ src/pstring.h
@@ -21,45 +21,15 @@ char* pact_string_copy_cstr(const pact_String* str);
 pact_String* pact_string_substr(const pact_String*str, const size_t start, const size_t end);
 int pact_string_compare(const pact_String* a, const pact_String* b);
 int pact_string_compare_cstr(const pact_String* a, const char* b);
-inline pact_String* pact_string_copy(const pact_String* str);
+pact_String* pact_string_copy(const pact_String* str);
 int pact_string_find_after_cstr(const pact_String* str, const char* value, const size_t offset);
-inline int pact_string_find_cstr(const pact_String* str, const char* value);
-inline int pact_string_find_after(const pact_String* str, const pact_String* value, const size_t offset);
-inline int pact_string_find(const pact_String* str, const pact_String* value);
-inline pact_String* pact_string_chop_front(const pact_String* str, const size_t length);
-inline pact_String* pact_string_chop_back(const pact_String* str, const size_t length);
+int pact_string_find_cstr(const pact_String* str, const char* value);
+int pact_string_find_after(const pact_String* str, const pact_String* value, const size_t offset);
+int pact_string_find(const pact_String* str, const pact_String* value);
+pact_String* pact_string_chop_front(const pact_String* str, const size_t length);
+pact_String* pact_string_chop_back(const pact_String* str, const size_t length);
 pact_String* pact_string_concat(const pact_String* a, const pact_String* b);
 pact_LinkedList* pact_string_split_cstr(const pact_String* str, const char* delim);
-inline pact_LinkedList* pact_string_split(const pact_String* str, const pact_String* delim);
-
-
-//allocation/destruction
-pact_String* pact_string_create(const char* data);
-void pact_string_destroy(pact_String* str);
-
-//operations
-int pact_string_clear(pact_String* str);
-int pact_string_assign(pact_String* str, const char* data);
-int pact_string_assign_length(pact_String* str, const char* data, unsigned int length);
-const char* pact_string_view(pact_String* str);
-int pact_string_retrieve(pact_String* str, char* data, unsigned int length);
-int pact_string_retrieve_substr(pact_String* str, char* data, unsigned int start, unsigned int length);
-unsigned int pact_string_length(pact_String* str);
-int pact_string_compare(pact_String* a, pact_String* b);
-int pact_string_compare_cstr(pact_String* a, char* b);
-int pact_string_clone(pact_String* src, pact_String* dst);
-int pact_string_clone_substr(pact_String* src, pact_String* dst, unsigned int start, unsigned int length);
-int pact_string_find(pact_String* str, pact_String* find);
-int pact_string_find_after(pact_String* str, pact_String* find, unsigned int start);
-int pact_string_find_cstr(pact_String* str, char* find);
-int pact_string_find_cstr_after(pact_String* str, char* find, unsigned int start);
-int pact_string_partition(pact_String* str, pact_String* delim, pact_String* head, pact_String* tail);
-int pact_string_partition_cstr(pact_String* str, char* delim, pact_String* head, pact_String* tail);
-int pact_string_append(pact_String* str, pact_String* tail);
-int pact_string_append_cstr(pact_String* str, char* tail);
-int pact_string_prepend(pact_String* str, pact_String* head);
-int pact_string_prepend_cstr(pact_String* str, char* head);
-//int pact_string_chop_front(pact_String* str, unsigned int chop);
-//int pact_string_chop_back(pact_String* str, unsigned int chop);
+pact_LinkedList* pact_string_split(const pact_String* str, const pact_String* delim);
 
 #endif
